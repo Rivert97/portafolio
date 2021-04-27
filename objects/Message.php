47f2135083728c3d0143198366e222a5d350abed
Rivert97 <betogarcia97@live.com.mx>
2021-04-27 15:32:40 +0000

    Se inicia con la implementación de envío de email. Todavía no funciona

--- objects/Message.php
+++ objects/Message.php
@@ -1,4 +1,9 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require_once "../vendor/autoload.php";
+
 class Message {
 	private $name;
 	private $mailphone;
@@ -50,6 +55,31 @@ class Message {
 		}
 		return $retval;
 
+	}
+
+	public function send_email() {
+        require_once '../conf/email.php';
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port = 587;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->SMTPAuth = True;
+		$mail->Username = "rgarciag.contacto@gmail.com";
+		$mail->Password = "Huevonivia97";
+		$mail->SetFrom("rgarciag.contacto@gmail.com", "Roberto Garcia");
+		$mail->AddAddress("betogarcia97@live.com.mx", "Roberto Garcia");
+		$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+		$mail->IsHTML(true);
+		$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+		$mail->MsgHTML($content);
+		if (!$mail->Send()) {
+			echo "Error while sending Email.";
+			var_dump($mail);
+		} else {
+			echo "Email sent successfully";
+		}
 	}
 }
 ?>
